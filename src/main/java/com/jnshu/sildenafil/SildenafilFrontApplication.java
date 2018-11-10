839d9286e5a2d00b9324dce540dc768b10dd6901
tme <tme1024723995@126.com>
2018-11-10 21:51:49 +0800

    Initial commit

---
+++ src/main/java/com/jnshu/sildenafil/SildenafilFrontApplication.java
@@ -0,0 +1,12 @@
+package com.jnshu.sildenafil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SildenafilFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SildenafilFrontApplication.class, args);
+    }
+}
